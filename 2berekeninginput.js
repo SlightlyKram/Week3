f865019ddbdb192d3013aea07b7aed8f490b2946
SlightlyKram <70692409+SlightlyKram@users.noreply.github.com>
2020-09-08 14:56:08 +0200

    added notes

--- 2berekeninginput.js
+++ 2berekeninginput.js
@@ -1,11 +1,13 @@
 
+//Salima Fong-Pien-Joe
+//Software Developer
 
-alert("input number")
+alert("input number") //popup om nummer in te voeren
 
-var ingevoerd = prompt("?");
+var ingevoerd = prompt("?"); //ingevulde getal
 
-var som = (ingevoerd + 6) * 10 / 5 - 12
+var som = (ingevoerd + 6) * 10 / 5 - 12 //hele som
 
-var answer = som
+var answer = som // antwoord op de som
 
-document.write("(? + 6) * 10 / 5 - 12" + " " + "=" + " " + answer); 
+document.write("(? + 6) * 10 / 5 - 12" + " " + "=" + " " + answer); // antwoord en berekening van de som
