6c6d77debdbdf867fdf6adbc41bb3a3a98bc4c57
SlightlyKram <70692409+SlightlyKram@users.noreply.github.com>
2020-09-08 14:42:22 +0200

    naam toegevoegd

---
+++ Tafel3.js
@@ -0,0 +1,11 @@
+
+//Salima Fong-Pien-Joe
+//Software Developer
+
+alert("Input number here");
+
+var bestaand = 3;
+var ingevoerd = prompt("6 * ?")
+var answer = bestaand * ingevoerd
+
+	document.write(answer)
