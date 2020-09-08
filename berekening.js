15341a785cbca41586501b152d052868b359b013
SlightlyKram <70692409+SlightlyKram@users.noreply.github.com>
2020-09-08 14:55:06 +0200

    added comments

--- berekening.js
+++ berekening.js
@@ -1,9 +1,11 @@
 
 
 
+//Salima Fong-Pien-Joe
+//Software Developer
 
-var som = (12 + 6) * 10 / 5 - 12
+var som = (12 + 6) * 10 / 5 - 12 //dit is de hele som
 
-var answer = som
+var answer = som // dit is het antwoord op de som
 
-	document.write("(12 + 6) * 10 / 5 - 12" + " " + "=" + " " + answer);
+	document.write("(12 + 6) * 10 / 5 - 12" + " " + "=" + " " + answer); // dit laat de berekening en het antwoord zien
