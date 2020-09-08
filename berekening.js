3f0a77ce0f1b0fcded992fed1a60d4666b25bdce
SlightlyKram <70692409+SlightlyKram@users.noreply.github.com>
2020-09-08 13:45:43 +0200

    berekening lange som toegevoegd

---
+++ berekening.js
@@ -0,0 +1,14 @@
+
+
+var basis = 12
+var optellen = 6
+var keer = 10
+var delen = 5
+var aftellen = 12
+var helft = basis + optellen
+var helft2 = helft * keer
+var som1 = helft2 / delen
+var som2 = som1 - aftellen
+var answer = som2
+
+	document.write(answer)
