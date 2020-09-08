821eaad07a11268242096129e58e851123bc4d86
SlightlyKram <70692409+SlightlyKram@users.noreply.github.com>
2020-09-08 14:25:23 +0200

    Added Calculations

---
+++ 2berekeninginput.js
@@ -0,0 +1,11 @@
+
+
+alert("input number")
+
+var ingevoerd = prompt("?");
+
+var som = (ingevoerd + 6) * 10 / 5 - 12
+
+var answer = som
+
+document.write("(? + 6) * 10 / 5 - 12" + " " + "=" + " " + answer); 
